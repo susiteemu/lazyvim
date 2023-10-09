35c42e82607ea0d96f1d1e04510736d30b1b3074
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2023-10-09 10:17:48 +0300

    Go auto-organize import removed + null-ls -> none-ls

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-    vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+    -- vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
     vim.lsp.buf.format()
   end,
 })
