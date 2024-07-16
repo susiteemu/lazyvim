a404480859312b88a3a9ea5f0ad30d46579197ff
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2024-07-16 19:39:52 +0300

    Added Telekasten

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,3 +27,19 @@ end, { desc = "Validate a Jenkinsfile" })
 map({ "n", "v" }, "'", '"0')
 map({ "n", "v" }, "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
 map({ "n", "v" }, "<leader>/", LazyVim.pick("live_grep", { root = false }), { desc = "Grep (cwd)" })
+
+-- Launch panel if nothing is typed after <leader>z
+map({ "n" }, "<leader>z", "<cmd>Telekasten panel<CR>")
+
+-- Most used functions
+map({ "n" }, "<leader>zf", "<cmd>Telekasten find_notes<CR>")
+map({ "n" }, "<leader>zg", "<cmd>Telekasten search_notes<CR>")
+map({ "n" }, "<leader>zd", "<cmd>Telekasten goto_today<CR>")
+map({ "n" }, "<leader>zz", "<cmd>Telekasten follow_link<CR>")
+map({ "n" }, "<leader>zn", "<cmd>Telekasten new_note<CR>")
+map({ "n" }, "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
+map({ "n" }, "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
+map({ "n" }, "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+
+-- Call insert link automatically when we start typing a link
+map({ "i" }, "[[", "<cmd>Telekasten insert_link<CR>")
