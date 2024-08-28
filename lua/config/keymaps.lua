9a3ea6105921e57911f19077b1cfc0b4158fb05b
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2024-08-28 06:29:09 +0300

    Use vim.keymap.set as Lazy suggests

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,17 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
+local map = vim.keymap.set
 
 map("n", "<leader>fc", "<cmd>let @+ = expand('%:p')<cr>", {
   desc = "Yank current buffer's absolute path",
@@ -21,9 +11,6 @@ map({ "n", "v" }, "<leader>gf", function()
 end, { desc = "Find file under cursor" })
 map({ "n", "v" }, "ga.", "<cmd>TextCaseOpenTelescope<CR>", { desc = "Text case conversions" })
 map({ "n", "v" }, "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
-map({ "n", "v" }, "<leader>jf", function()
-  require("jenkinsfile_linter").validate()
-end, { desc = "Validate a Jenkinsfile" })
 map({ "n", "v" }, "'", '"0')
 map({ "n", "v" }, "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
 map({ "n", "v" }, "<leader>/", LazyVim.pick("live_grep", { root = false }), { desc = "Grep (cwd)" })
