59432ea8087a1dfab402cd8a17c20660026b4c5d
Teemu Turunen <teemu.turunen@hiq.fi>
2024-12-25 11:03:21 +0200

    Customize lualine

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -340,4 +340,15 @@ return {
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "￨", right = "￨" },
+        section_separators = { left = "", right = "" },
+      },
+    },
+  },
 }
